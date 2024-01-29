bea9c837fb7925623d49c59fa4e5da2682adad4f
avabellaw <70448139+avabellaw@users.noreply.github.com>
2024-01-29 22:12:01 +0000

    Add model for Recipes table

--- myrecipe/models.py
+++ myrecipe/models.py
@@ -5,3 +5,17 @@ class Users(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(25), unique=True, nullable=False)
     password = db.Column(db.String(), nullable=False)
+    
+    def __repr__(self):
+        return f"User {self.id} - {self.username}"
+    
+class Recipes(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    title = db.Column(db.String(20), nullable=False)
+    desc = db.Column(db.String(200), nullable=False)
+    ingredients = db.Column(db.String(500), nullable=False)
+    instructions = db.Column(db.String(1000), nullable=False)
+    
+    def __repr__(self):
+        return f"Recipe {self.id} - {self.title}"
