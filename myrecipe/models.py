162de664b351052334463ab0d61a99be57c9d1f7
avabellaw <70448139+avabellaw@users.noreply.github.com>
2024-01-29 14:46:56 +0000

    Add model for Users and create the table

--- myrecipe/models.py
+++ myrecipe/models.py
@@ -1,1 +1,7 @@
 from myrecipe import db
+from sqlalchemy.orm import Mapped, mapped_column
+
+class Users(db.Model):
+    id: Mapped[int] = mapped_column(db.Integer, primary_key=True)
+    username: Mapped[str] = mapped_column(db.String(25), unique=True, nullable=False)
+    password: Mapped[str] = mapped_column(db.String(), nullable=False)
