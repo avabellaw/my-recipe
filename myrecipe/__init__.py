f2a509a42d4082ac9570569505cbd14c06b10db4
avabellaw <70448139+avabellaw@users.noreply.github.com>
2024-02-18 15:46:38 +0000

    Change postgres uri when in production

--- myrecipe/__init__.py
+++ myrecipe/__init__.py
@@ -14,7 +14,13 @@ UPLOAD_FOLDER = "image-uploads"
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+if os.environ.get("DEVELOPMENT") == "True":
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+else:
+    uri = str(os.environ.get("DATABASE_URL"))
+    if uri.startswith("postgres://"):
+        uri = uri.replace("postgres://", "postgresql://", 1)
+    app.config["SQLALCHEMY_DATABASE_URI"] = uri
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['PACKAGE_NAME'] = PACKAGE_NAME
 
@@ -23,5 +29,3 @@ db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
 from myrecipe import routes
-
-
