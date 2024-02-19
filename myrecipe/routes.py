00ee323d49e3c9223bb1722a3cbcd8a2cdf5eb3c
avabellaw <70448139+avabellaw@users.noreply.github.com>
2024-02-19 22:21:24 +0000

    Save changes to recipes + create unique image names
    Also deletes images now which is why having a unique name is needed

--- myrecipe/routes.py
+++ myrecipe/routes.py
@@ -155,13 +155,37 @@ def edit_recipe(recipe_id):
     add_dietary_tags_to_recipes([recipe])
     if user_owns_recipe(current_user.id, recipe): #type: ignore
         form = AddRecipeForm()
+        if request.method == "POST":
+            if form.validate_on_submit():
+                title = form.title.data
+                desc = form.desc.data 
+                ingredients = form.ingredients.data
+                instructions = form.instructions.data
+                
+                if title != recipe.title:
+                    recipe.title = title
+                if desc != recipe.desc:
+                    recipe.desc = desc
+                if ingredients != recipe.ingredients:
+                    recipe.ingredients = ingredients
+                if instructions != recipe.instructions:
+                    recipe.instructions = instructions
+                image = form.image.data
+                if image.filename != recipe.image_url.split("/")[-1]:
+                    if image:
+                        delete_image(recipe.image_url)
+                        recipe.image_url = save_image_locally(image)
+                db.session.add(recipe)
+                db.session.commit()
+                return redirect(url_for("view_recipe", recipe_id=recipe.id))
+            return render_template("edit-recipe.html", form=form)
         set_default_dietary_tags(form, dietary_tag_bools_to_data(get_recipe_dietary_tags_bools(recipe)))
         form.title.data = recipe.title # type: ignore
         form.desc.data = recipe.desc # type: ignore
         form.ingredients.data = recipe.ingredients # type: ignore
         form.instructions.data = recipe.instructions # type: ignore
         form.image.data = recipe.image_url # type: ignore
-        return render_template("edit-recipe.html", form=form)    
+        return render_template("edit-recipe.html", form=form, recipe=recipe)    
     flash("You can only edit your own recipes.", "danger")
     return redirect(url_for("my_recipes"))
 
@@ -170,7 +194,9 @@ def edit_recipe(recipe_id):
 @login_required
 def delete_recipe(recipe_id):
     recipe = get_recipe(recipe_id)
-    if user_owns_recipe(current_user.id, recipe): #type: ignore
+    if user_owns_recipe(current_user.id, recipe):
+        if recipe.image_url and os.path.exists(app.config["PACKAGE_NAME"] + "/" + recipe.image_url):
+            delete_image(recipe.image_url) 
         db.session.delete(recipe)
         db.session.commit()
         flash(f'Recipe "{recipe}" deleted.', "success")
@@ -308,9 +334,25 @@ def save_image_locally(image):
     save_path = app.config["PACKAGE_NAME"] + "/" + app.config['UPLOAD_FOLDER']
     if not os.path.exists(save_path):
         os.makedirs(save_path)
+        
+    counter = 2
+    # If file with same name exists, add a number to the end of the filename and increment until a unique filename is found
+    while os.path.isfile(os.path.join(save_path, filename)):
+        filename_parts = filename.split(".")
+        filename_name = filename_parts[0]
+        if "_" in filename_name:
+            filename_name = filename_name.split("_")[0] + f"_{counter}"
+        else:
+            filename_name = filename_name + "_2"
+        filename = f'{filename_name}.{".".join(filename_parts[1:])}'
+        counter+=1
+
     image.save(os.path.join(save_path, filename))
                 
     return "/" + app.config["UPLOAD_FOLDER"] + "/" + filename 
+
+def delete_image(image_url):
+    os.remove(os.path.join(app.config["PACKAGE_NAME"] + "/" + image_url))
 
 def user_owns_recipe(user_id, recipe):
     if hasattr(recipe, "modified_by_id"):
