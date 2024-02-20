0f16b76592027a699bb8e683564eac534a2fb6a7
avabellaw <70448139+avabellaw@users.noreply.github.com>
2024-02-20 16:30:48 +0000

    Test uploading image to bucket

--- myrecipe/routes.py
+++ myrecipe/routes.py
@@ -118,7 +118,8 @@ def add_recipe():
         if form.validate_on_submit():
             image = form.image.data
             if image:
-                image_url = save_image_locally(image)
+                # image_url = save_image_locally(image)
+                upload_file(image, 'cloud-cube-eu2')
                 
             dietary_tags_id = add_dietary_tags_to_db(form.dietary_tags.data)
             recipe = Recipe(user_id=current_user.id, title=title, desc=desc, ingredients=ingredients, instructions=instructions, image_url=image_url if image else null(), dietary_tags_id=dietary_tags_id) # type: ignore
@@ -488,6 +489,28 @@ def update_dietary_tags(dietary_tags_id, dietary_tags_data):
         dietary_tags.is_egg_free = "ef" in form.dietary_tags.data
         db.session.add(dietary_tags)
         db.session.commit()
+        
+def upload_file(file_name, bucket, object_name=None):
+    """Upload a file to an S3 bucket
+
+    :param file_name: File to upload
+    :param bucket: Bucket to upload to
+    :param object_name: S3 object name. If not specified then file_name is used
+    :return: True if file was uploaded, else False
+    """
+
+    # If S3 object_name was not specified, use file_name
+    if object_name is None:
+        object_name = os.path.basename(file_name)
+
+    # Upload the file
+    s3_client = boto3.client('s3')
+    try:
+        response = s3_client.upload_file(file_name, bucket, object_name)
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
 
 # Wtforms
 
