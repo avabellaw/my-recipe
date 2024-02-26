6d346525e4ea490d6017da81b74dd93f982150ca
avabellaw <70448139+avabellaw@users.noreply.github.com>
2024-02-26 13:15:21 +0000

    Move init dietary tags + img validation into js file

--- myrecipe/static/js/load-preview-image-function.js
+++ myrecipe/static/js/load-preview-image-function.js
@@ -1,3 +1,4 @@
+// Load the function to show preview of uploaded images
 function previewImage(event) {
     // Preview image before upload from StackOverflow[https://stackoverflow.com/questions/4459379/preview-an-image-before-it-is-uploaded]
     let output = document.getElementById('recipe-header_image');
