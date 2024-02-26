9f808763e39326c3aed0a1ae0fa33274572b3a97
avabellaw <70448139+avabellaw@users.noreply.github.com>
2024-02-26 12:50:59 +0000

    Move load_preview_image_function macro to js file

---
+++ myrecipe/static/js/load-preview-image-function.js
@@ -0,0 +1,9 @@
+function previewImage(event) {
+    // Preview image before upload from StackOverflow[https://stackoverflow.com/questions/4459379/preview-an-image-before-it-is-uploaded]
+    let output = document.getElementById('recipe-header_image');
+    output.classList.remove('hide');
+    output.src = URL.createObjectURL(event.target.files[0]);
+    output.onload = function () {
+        URL.revokeObjectURL(output.src) // free memory
+    }
+};
